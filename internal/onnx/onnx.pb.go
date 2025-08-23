bb50aed1e913f6d37cf914e58cac64b21e8a507f
David Ndungu <ndungu.sink@gmail.com>
2025-08-23 08:26:42 -0700

    fix(onnx): Apply linting and formatting fixes to generated protobuf file

--- internal/onnx/onnx.pb.go
+++ internal/onnx/onnx.pb.go
@@ -13,11 +13,12 @@
 package onnx
 
 import (
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
 	sync "sync"
 	unsafe "unsafe"
+
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 const (
