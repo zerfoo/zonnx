543121073333ff411f61c0a4ecb21a8fc0e0ca9b
David Ndungu <ndungu.sink@gmail.com>
2025-08-25 00:11:21 -0700

    pkg/importer/layers: cleanup in relu.go and reshape.go (no functional changes)

--- pkg/importer/layers/relu.go
+++ pkg/importer/layers/relu.go
@@ -1,25 +1,19 @@
 package layers
 
 import (
-	"github.com/zerfoo/zerfoo/compute"
-	"github.com/zerfoo/zerfoo/graph"
-	"github.com/zerfoo/zerfoo/layers/activations"
-	"github.com/zerfoo/zerfoo/numeric"
-	"github.com/zerfoo/zerfoo/tensor"
 	"github.com/zerfoo/zonnx/internal/onnx"
 	"github.com/zerfoo/zonnx/pkg/registry"
 )
 
 func init() {
-	registry.Register("Relu", BuildReLU[float32])
+	registry.Register("Relu", BuildReLU)
 }
 
 // BuildReLU creates a new ReLU layer from an ONNX node.
-func BuildReLU[T tensor.Numeric](
-	engine compute.Engine[T],
-	ops numeric.Arithmetic[T],
+func BuildReLU(
 	_ *onnx.NodeProto,
 	_ *registry.ConversionContext,
-) (graph.Node[T], error) {
-	return activations.NewReLU[T](engine, ops), nil
+) (interface{}, error) {
+	// TODO: Return a ZMF representation of ReLU
+	return nil, nil
 }
